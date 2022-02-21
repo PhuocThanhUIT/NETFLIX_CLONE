45758a5bece253ea3a27b27d8b8b562af7c20493
Hứa Phước Thanh <17521046@ms.uit.edu.vn>
2022-02-21 17:34:23 +0700

    React router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,35 @@ import Home from './pages/home/Home';
 import "./app.scss"
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 const App = () => {
-  return <Register/>;
+  const user = true;
+  return (
+    <BrowserRouter>
+    <Routes>
+      {user && (
+        <>
+        <Route path="/movies" element={<Home type="movies" />}/>
+      
+        <Route path="/series" element={<Home type="series" />}/>
+  
+        <Route path="/watch" element={<Watch />}/>
+        </>
+      )}
+      <Route path="/"  element={user? <Home />:<Register/>}/>
+    
+      <Route path="/register" element={!user?<Register />:<Home/>}/>
+
+      <Route path="/login" element={!user?<Login />:<Home/>}/>
+      
+    </Routes>
+  </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./navbar.scss";
 import {Search, Notifications, ArrowDropDown} from '@material-ui/icons';
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
 const Navbar = () => {
     const [isScrolled,setIsCrolled] = useState(false);
     window.onscroll = () => {
@@ -12,9 +13,15 @@ const Navbar = () => {
         <div className="container">
             <div className="left">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
+                <Link to = "/" className="link">
                 <span>Homepage</span>
-                <span>Series</span>
-                <span>Movies</span>
+                </Link>
+                <Link to = "/series" className="link">
+                    <span>Series</span>
+                </Link>
+                <Link to = "/movies" className="link">
+                    <span>Movies</span>
+                </Link>
                 <span>New and Popular</span>
                 <span>My List</span>
             </div>
