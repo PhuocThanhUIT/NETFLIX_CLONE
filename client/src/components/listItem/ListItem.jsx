44bdb88bc8a3d1bce23428d1256bb7163de02666
Hứa Phước Thanh <17521046@ms.uit.edu.vn>
2022-02-21 14:31:21 +0700

    full-screen video

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -17,7 +17,7 @@ export default function ListItem({index}) {
         <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
         {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop={true} />
+          <video src={trailer} autoPlay={true} loop={true} muted={true} />
           <div className="itemInfo">
             <div className="icons">
               <PlayArrow className="icon" />
