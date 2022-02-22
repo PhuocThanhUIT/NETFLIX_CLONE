9f7a59d4b7bc0692b12cdee8c60b7d41b155f7bf
Hứa Phước Thanh <17521046@ms.uit.edu.vn>
2022-02-22 15:29:25 +0700

    react admin page

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,7 +1,26 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import "./featured.scss";
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+
 
 function Featured({type}) {
+  const [content,setContent]=useState({});
+  useEffect(() =>{
+    const getRandomContent = async () => {
+      try{
+        const res = await axios.get(`movies/random?type=${type}`,{
+          headers:{
+            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTdjMDZjMTY2MWRmZjRkZmVmYTA5MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTQ1NDM2MiwiZXhwIjoxNjQ1NTQwNzYyfQ.gqceBd32dmB0ofAjhp5f0_bCw7LinHFYYcByvCtTRdk"
+          }
+        });
+        setContent(res.data[0]);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    getRandomContent();
+  },[type]);
   return (
     <div className = "featured">
         {type && (
@@ -25,10 +44,10 @@ function Featured({type}) {
           </select>
         </div>
       )}
-        <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+        <img src={content.img} alt="" />
         <div className="info">
-            <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
-            <span className="desr">Lorem, ipsum dolor sit amet consectetur adipisicing elit. A tenetur amet excepturi veniam aliquid, molestiae iure quas suscipit, totam facere laborum architecto nam earum repudiandae voluptates delectus vitae rem reprehenderit.</span>
+            <img src={content.imgTitle} alt="" />
+            <span className="desr">{content.desc}</span>
             <div className="buttons">
                 <button className="play">
                     <PlayArrow/>
